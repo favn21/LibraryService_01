7389b2b359f552e85c5e2aadaa09eb5c2d438d18
nadya <tutu87238@gmail.com>
2024-07-15 16:21:50 +0700

    feat:  Изменение класса TokenService, исправление замечаний.

--- src/main/java/com/example/api/service/TokenService.java
+++ src/main/java/com/example/api/service/TokenService.java
@@ -5,12 +5,18 @@ import com.example.api.models.response.TokenResponse;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TokenService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
     private static final String AUTH_URL = "http://localhost:8080/auth/login";
 
     public static String getAuthToken(String login, String password) {
+
+
+        logger.debug("Sending authentication request for login: {}", login);
 
         TokenRequest authRequest = new TokenRequest(login, password);
 
@@ -18,12 +24,16 @@ public class TokenService {
                 .contentType(ContentType.JSON)
                 .body(authRequest)
                 .when()
-                .post(AUTH_URL)
-                .then().log().all()
-                .extract()
-                .response();
+                .get(AUTH_URL)
+                .then().extract().response();
 
-        TokenResponse tokenResponse = response.as(TokenResponse.class);//
+        if (response.statusCode() != 200) {
+            logger.warn("Failed to authenticate: {}", response.getStatusLine());
+            throw new RuntimeException("Failed to authenticate: " + response.getStatusLine());
+        }
+
+        TokenResponse tokenResponse = response.as(TokenResponse.class);
+        logger.info("Successfully authenticated. Token: {}", tokenResponse.getToken());
         return tokenResponse.getToken();
     }
 }
